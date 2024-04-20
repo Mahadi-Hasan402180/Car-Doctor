c15e8487f38dc3ff6722e133bf7af6908aa364b5
Mahadi-Hasan402180 <mahadimhf@gmail.com>
2024-04-20 19:31:09 +0600

    added server some instument

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 
 const ServiceCard = ({service}) => {
-    const{title,img,price} = service;
+    const{_id,title,img,price} = service;
   return (
     <div className="card w-4/3   bg-base-100 shadow-xl">
       <figure>
@@ -17,6 +18,11 @@ const ServiceCard = ({service}) => {
           <div className="font-bold">{title}</div>
         </h2>
         <p className=" font-bold text-blue-600">Price: ${price}</p>
+        <div className="card-actions">
+            <Link to = {`/checkout/${_id}`}>
+            <button className="btn btn-primary">Book Now</button>
+            </Link>
+        </div>
         
       </div>
     </div>
