57d6f5cc8b2040437d1384583ba96b799bb548df
Mahadi-Hasan402180 <mahadimhf@gmail.com>
2024-04-22 11:36:25 +0600

    complete services deleted operation

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,34 +1,46 @@
+const BookingRow = ({ booking ,handleDelete}) => {
+  const { CustomerName, price, service, CustomerPhone, email, date, img,_id } = booking;
+     
+    
 
+  return (
+    <tr>
+      <th>
+        <button onClick={()=>handleDelete(_id)} className="btn btn-sm btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </th>
 
-const BookingRow = ({booking}) => {
-    const{CustomerName,price,service,CustomerPhone,email,date,img} = booking;
-    return (
-        <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                  <div className="avatar">
-                    <div className="rounded w-24 h-24">
-                      <img
-                        src={img}
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>   
-                </div>
-              </td>
+      <td>
+        <div className="avatar">
+          <div className="rounded w-24 h-24">
+            <img src={img} alt="Avatar Tailwind CSS Component" />
+          </div>
+        </div>
+      </td>
 
-              <td>{service}</td>
-              <td>{CustomerPhone}</td>
-              <td>${price}</td>
-              <td>{date}</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">Pending</button>
-              </th>
-            </tr>
-    );
+      <td>{service}</td>
+      <td>{CustomerPhone}</td>
+      <td>${price}</td>
+      <td>{date}</td>
+      <th>
+        <button className="btn btn-ghost btn-xs">Pending</button>
+      </th>
+    </tr>
+  );
 };
 
 export default BookingRow;
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -13,6 +13,28 @@ const Bookings = () => {
       .then((data) => setBookings(data));
   }, []);
 
+
+  const handleDelete = id =>{
+    const proceed = confirm('are you sure want to delete');
+    if(proceed){
+      fetch(`http://localhost:5000/bookings/${id}`,{
+        method:'DELETE'
+      })
+      .then(res=>res.json())
+      .then(data=>{console.log(data);
+        if(data.deletedCount>0){
+          alert('Deleted Successfully')
+          const remaining = bookings.filter(booking=>booking._id !==id)
+          setBookings(remaining);
+        }
+
+      
+      })
+
+      
+    }
+   }
+
   return (
     <div>
       <h2 className="text-3xl">Your Bookings:{bookings.length}</h2>
@@ -23,9 +45,7 @@ const Bookings = () => {
           <thead>
             <tr>
               <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
+                Delete
               </th>
               <th>Image</th>
               <th>Service Name</th>
@@ -41,6 +61,7 @@ const Bookings = () => {
 
                 key = {booking._id}
                 booking = {booking}
+                handleDelete ={handleDelete}
             ></BookingRow>)}
            
            
